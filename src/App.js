6c03f9697b9405e379f95aab1355689741adddff
Kasunjith-Bimal <kasunysoft@gmail.com>
2018-07-21 21:55:07 +0530

    add Seach Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Search from './Component/Search'
 class App extends Component {
   render() {
     return (
       <div className="container">
        <div className="col-lg-8">
-       <form >
-  <div className="form-group">
-    <label htmlFor="email">Search Place</label>
-    <input type="text" className="form-control" id="email"/>
-  </div>
-  
-  <button type="submit" className="btn btn-default">Search</button>
-</form>
-<ul>
-<li> Kandy </li>
-<li> Colombo </li>
-  </ul>
+       <Search/>
+         <ul>
+            <li> Kandy </li>
+            <li> Colombo </li>
+        </ul>
        </div>
        <div className="col-lg-4">
        <h1 className="form-control">Saved place </h1>
