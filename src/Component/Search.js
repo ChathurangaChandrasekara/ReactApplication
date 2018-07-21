27bf878973e5ce57923163d7a76515c3eac12177
Kasunjith-Bimal <kasunysoft@gmail.com>
2018-07-21 23:21:25 +0530

    search function add

--- src/Component/Search.js
+++ src/Component/Search.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 
 class Search extends Component{
+
+ SearchHadelar = (e) => {
+e.preventDefault();
+console.log("clicked");
+const keyType = this.refs.inputword.value;
+console.log(keyType);
+ }   
+
 render(){
 return(
     <form >
     <div className="form-group">
       <label htmlFor="email">Search Place</label>
-      <input type="text" className="form-control" id="email"/>
+      <input type="text" className="form-control" ref="inputword"/>
     </div>
     
-    <button type="submit" className="btn btn-default">Search</button>
+    <button onClick={this.SearchHadelar.bind(this)} className="btn btn-default">Search</button>
   </form>
  
 );
