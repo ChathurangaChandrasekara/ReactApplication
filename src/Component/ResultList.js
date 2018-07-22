396a2a8996c8239c3efdcf13aab1ab9f40b7edff
Kasunjith Bimal <kasunysoft@gmail.com>
2018-07-22 21:30:26 +0530

    Show Result list Search

--- src/Component/ResultList.js
+++ src/Component/ResultList.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 
 class ResultList extends Component{
 render(){
+    console.log(this.props.resultPlace);
+    if(this.props.resultPlace.results !== undefined){
+        alert("AAAA")
+        console.log(this.props.resultPlace);
+     var placeList = this.props.resultPlace.results.map(name =>{
+         console.log(placeList);
+    return(
+            <li key={name.id}> {name.name} </li>
+    )
+     })
+    }
+    
 return(
 <ul>
-    <li> Kandy </li>
-    <li> Colombo </li>
+   {placeList}
 </ul>
  
 );
