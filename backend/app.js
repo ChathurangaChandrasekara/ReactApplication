ce4d677df2dbd6c5086427017b0d7c98204b8496
Kasunjith Bimal <kasunysoft@gmail.com>
2018-07-22 13:17:22 +0530

    Add backend for node

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+const app = express();
+
+app.listen(3001,()=>{
+
+    console.log("Listing post 3001");
+})
